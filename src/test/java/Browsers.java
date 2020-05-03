0e6ae994d5b50380c085df837c349a9f6ae194f4
Vasiliy.Omelchuk <vasoenergi@gmail.com>
2020-05-03 18:27:11 +0300

    [master] add cross-browser test,testNGxml,update POMxml

--- src/test/java/Browsers.java
+++ src/test/java/Browsers.java
@@ -1,3 +1,8 @@
-public class Browsers {
+public enum Browsers {
+    CHROME,
+    FIREFOX,
+    IE11,
+    SAFARI,
+    EDGE
 
 }
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,9 +1,13 @@
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-import io.github.bonigarcia.wdm.DriverManagerType;
+import com.sun.org.apache.xalan.internal.xsltc.dom.SimpleResultTreeImpl;
+import io.github.bonigarcia.wdm.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import pages.ElementsHelper;
 import pages.HeaderTab;
 import pages.LoginPage;
@@ -16,22 +20,46 @@ public class TestBase {
     HeaderTab headerTab = new HeaderTab();
     LoginPage loginPage = new LoginPage();
     SettingPage settingPage = new SettingPage();
+    String testURL = "https://github.com/login";
+    String login = "vasoenergi@gmail.com";
+    String pass = "FreeR1de!";
+
     @BeforeClass
-    public void setUp() {
-        ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
-        driver = new ChromeDriver();
+    @Parameters ("browser")
+    public void setUp(Browsers browser) {
+        switch (browser) {
+            case CHROME:
+                ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
+                driver = new ChromeDriver();
+                break;
+            case EDGE:
+                EdgeDriverManager.getInstance(DriverManagerType.EDGE).setup();
+                driver = new EdgeDriver();
+                break;
+            case IE11:
+                InternetExplorerDriverManager.getInstance(DriverManagerType.IEXPLORER).setup();
+                driver = new InternetExplorerDriver();
+                break;
+            case FIREFOX:
+                FirefoxDriverManager.getInstance(DriverManagerType.FIREFOX).setup();
+                driver = new FirefoxDriver();
+                break;
+            default:
+                throw new RuntimeException("Invalid specified browser: " + browser + "expected one of 'CHROME','IE11','EDGE','FIREFOX'");
+        }
+
         elementsHelper = new ElementsHelper(driver);
-        driver.navigate().to("https://github.com/login");
+        driver.navigate().to(testURL);
         driver.manage().window().maximize();
-        driver.findElement(loginPage.loginInputField).sendKeys("vasoenergi@gmail.com");
-        driver.findElement(loginPage.passwordInputField).sendKeys("FreeR1de!");
+        driver.findElement(loginPage.loginInputField).sendKeys (login);
+        driver.findElement(loginPage.passwordInputField).sendKeys(pass);
         driver.findElement(loginPage.signInButton).click();
     }
 
     @AfterClass
     public void tearDown() {
         //if (driver !=null)
-            driver.quit();
+        driver.quit();
     }
 
 }
